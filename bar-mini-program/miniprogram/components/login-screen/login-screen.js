a76e9a189f2bc88831f32d11634b9317a0724915
PhoebeAkle <xiewenxuan02@outlook.com>
2025-06-17 17:12:23 +0800

    有登陆界面
    有小程序登陆界面无一级界面

---
+++ bar-mini-program/miniprogram/components/login-screen/login-screen.js
@@ -0,0 +1,97 @@
+Component({
+  properties: {
+    onLogin: {
+      type: Function
+    },
+    onSkip: {
+      type: Function
+    }
+  },
+
+  data: {
+    email: '',
+    phone: '',
+    verificationCode: '',
+    countdown: 0,
+    error: '',
+    activeTab: 'email'
+  },
+
+  methods: {
+    handleEmailLogin(e) {
+      const { email } = this.data
+      if (!email) {
+        this.setData({ error: '请输入邮箱地址' })
+        return
+      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if (!emailRegex.test(email)) {
+        this.setData({ error: '请输入有效的邮箱地址' })
+        return
+      }
+      this.setData({ error: '' })
+      this.triggerEvent('login')
+    },
+
+    handlePhoneLogin(e) {
+      const { phone, verificationCode } = this.data
+      if (!phone) {
+        this.setData({ error: '请输入手机号码' })
+        return
+      }
+      if (!/^1[3-9]\d{9}$/.test(phone)) {
+        this.setData({ error: '请输入有效的中国大陆手机号码' })
+        return
+      }
+      if (!verificationCode) {
+        this.setData({ error: '请输入验证码' })
+        return
+      }
+      this.setData({ error: '' })
+      this.triggerEvent('login')
+    },
+
+    sendVerificationCode() {
+      const { phone } = this.data
+      if (!phone) {
+        this.setData({ error: '请输入手机号码' })
+        return
+      }
+      if (!/^1[3-9]\d{9}$/.test(phone)) {
+        this.setData({ error: '请输入有效的中国大陆手机号码' })
+        return
+      }
+
+      this.setData({ error: '', countdown: 60 })
+      const timer = setInterval(() => {
+        if (this.data.countdown <= 1) {
+          clearInterval(timer)
+          this.setData({ countdown: 0 })
+          return
+        }
+        this.setData({ countdown: this.data.countdown - 1 })
+      }, 1000)
+    },
+
+    onEmailInput(e) {
+      this.setData({ email: e.detail.value })
+    },
+
+    onPhoneInput(e) {
+      const value = e.detail.value.replace(/\D/g, '')
+      this.setData({ phone: value })
+    },
+
+    onCodeInput(e) {
+      this.setData({ verificationCode: e.detail.value })
+    },
+
+    onTabChange(e) {
+      this.setData({ activeTab: e.detail.value })
+    },
+
+    onSkip() {
+      this.triggerEvent('skip')
+    }
+  }
+}) 
